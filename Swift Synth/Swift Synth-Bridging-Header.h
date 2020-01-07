fdfa281bbac908b3bfd7ee78bc068805d9b8948c
Atharva Vaidya <atharva.vaidya@icloud.com>
2020-01-07 21:13:27 +0530

    Removed Swift implementation for Oscillator and Synth

--- Swift Synth/Swift Synth-Bridging-Header.h
+++ Swift Synth/Swift Synth-Bridging-Header.h
@@ -2,3 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "Waveform.h"
+#import "Signal.h"
+#import "Synth.h"
+#import "Oscillator.h"
